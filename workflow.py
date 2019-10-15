0afd7ae489c843dc519cf29b5db87fc2b3d59e59
Leandro Aluisio Scholz <leandro.a.scholz@gmail.com>
2019-10-15 19:25:19 +1000

    final changes to be tested locally during Porto Taggathon

--- workflow.py
+++ workflow.py
@@ -11,8 +11,8 @@ from swc_to_tiff_stack import swc_to_tiff_stack
 import tifffile as tiff
 
 
-def workflow(in_images, out_path):
-    print("Starting workflow!")
+def workflow(in_images, out_path, param_threshold_value, param_auto_downsampled):
+    print('Starting workflow!')
     
     for neubias_input_image in in_images:
         print('---------------------------')
@@ -27,27 +27,33 @@ def workflow(in_images, out_path):
         img = skimage.external.tifffile.imread(file_path)
         img = np.flip(img,axis=1)
         skimage.external.tifffile.imsave(out_file_path,img)
-        print("Finished running: 180 degrees image rotation in xy axis")
+        print('Finished running: 180 degrees image rotation in xy axis')
         
+        #parameters 
+        if param_auto_downsampled is True:
+            param = '-p NULL 0 ' + param_threshold_value + ' 1'
+        elif param_auto_downsampled is False:
+            param = '-p NULL 0 ' + param_threshold_value + ' 0'
+
         #Compute the neuron tracing
-        command = "/usr/bin/xvfb-run Vaa3D_CentOS_64bit_v3.458/vaa3d -x libvn2 -f app1 -i " + \
-            out_file_path + " -o " + out_file_path[:-4]+ ".swc"
-        return_code = call(command, shell=True, cwd="/") # waits for the subprocess to return
+        command = '/usr/bin/xvfb-run Vaa3D_CentOS_64bit_v3.458/vaa3d -x libvn2 -f app1 ' + param + ' -i ' + \
+            out_file_path + ' -o ' + out_file_path[:-4]+ '.swc'
+        return_code = call(command, shell=True, cwd='/') # waits for the subprocess to return
         time.sleep(10)#Wait 10 seconds because it can't process all the images for some reason
         #os.system(command)
-        print("Finished running filament tracing :"+command)
+        print('Finished running filament tracing :'+command)
         #im_size = imgio.imread(os.path.join(out_path, filename)).shape
         im_size = imgio.volread(out_file_path).shape #Size is Depth,Height,Width
         im_size = im_size[::-1] #Invert the size order to Width,Height,Depth
         print(im_size)
         #Rename the swc file form *.tif_ini.swc to *.swc
         #Needed for some vaa3d workflow where the output path is not taken into account.
-        #os.rename(out_file_path[:-4]+".tif_ini.swc", out_file_path[:-4]+ ".swc")
-        print("Run:"+' swc_to_tiff_stack('+ out_file_path[:-4] +'.swc, '+ out_path +','+ str(im_size)+')')
+        #os.rename(out_file_path[:-4]+'.tif_ini.swc', out_file_path[:-4]+ '.swc')
+        print('Run:'+' swc_to_tiff_stack('+ out_file_path[:-4] +'.swc, '+ out_path +','+ str(im_size)+')')
         
         # Convert the .swc tracing result to tiff stack files
-        swc_to_tiff_stack(out_file_path[:-4]+ ".swc", out_path, im_size)
-        print("Finished running conversion of swc to tiff stack")
+        swc_to_tiff_stack(out_file_path[:-4]+ '.swc', out_path, im_size)
+        print('Finished running conversion of swc to tiff stack')
         #TODO: error handling...
         
-    print("Done")    
+    print('Done')    
--- wrapper.py
+++ wrapper.py
@@ -1,36 +1,37 @@
 import sys
 import os
 import glob
-from subprocess import call
+
 from cytomine.models import Job
 from neubiaswg5 import CLASS_TRETRC
-from neubiaswg5.helpers import NeubiasJob, prepare_data, upload_data, upload_metrics
+from neubiaswg5.helpers import NeubiasJob, prepare_data, upload_data, upload_metrics, get_discipline
 from workflow import workflow 
 
 def main(argv):
     # 0. Initialize Cytomine client and job
     with NeubiasJob.from_cli(argv) as nj:
-        nj.job.update(status=Job.RUNNING, progress=0, statusComment="Initialisation...")
+        nj.job.update(status=Job.RUNNING, progress=0, statusComment='Initialisation...')
         problem_cls = CLASS_TRETRC
         is_2d = False
-        
+        threshold_value = nj.parameters.threshold_value
+        auto_downsampled = nj.parameters.auto_downsampled
         # 1. Create working directories on the machine
         # 2. Download the images
         in_images, gt_images, in_path, gt_path, out_path, tmp_path = prepare_data(problem_cls, nj, is_2d=is_2d, **nj.flags)
         
         # 3. Call the image analysis workflow using the run script
-        nj.job.update(progress=25, statusComment="Launching workflow...")
-        workflow(in_images,out_path)
+        nj.job.update(progress=25, statusComment='Launching workflow...')
+        workflow(in_images,out_path, threshold_value, auto_downsampled)
 
         #if return_code != 0:
-        #   err_desc = "Failed to execute the Vaa3D (return code: {})".format(return_code)
+        #   err_desc = 'Failed to execute the Vaa3D (return code: {})'.format(return_code)
         #nj.job.update(progress=50, statusComment=err_desc)
         #    raise ValueError(err_desc)       
         print('files in out_path '+ out_path +': ')
         for file in glob.glob(out_path+'/*'):
             print(file)
 
-        #files = (glob.glob(in_path+"/*.tif"))
+        #files = (glob.glob(in_path+'/*.tif'))
         #print('Removing flipped images...')
         #for i in range(0,len(files)):
         #    files[i] = files[i].replace('/in/','/out/')
@@ -47,9 +48,9 @@ def main(argv):
         })
         
         #5. Compute and upload the metrics
-        nj.job.update(progress=80, statusComment="Computing and uploading metrics (if necessary)...")
+        nj.job.update(progress=80, statusComment='Computing and uploading metrics (if necessary)...')
         upload_metrics(problem_cls, nj, in_images, gt_path, out_path, tmp_path, **nj.flags)
-        nj.job.update(status=Job.TERMINATED, progress=100, statusComment="Finished.")
+        nj.job.update(status=Job.TERMINATED, progress=100, statusComment='Finished.')
 
 if __name__ == "__main__":
     main(sys.argv[1:])
